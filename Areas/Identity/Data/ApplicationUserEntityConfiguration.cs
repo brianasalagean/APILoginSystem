0da0c8e3aeae769a2c965ac8f97170bdbcb2154d
brianasalagean <briana.salagean@stud.ubbcluj.ro>
2023-02-23 20:14:22 +0200

    Add project files.

---
+++ Areas/Identity/Data/ApplicationUserEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace APILoginSystem2.Areas.Identity.Data
+{
+    internal class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.Property(user => user.FirstName).HasMaxLength(255);
+            builder.Property(user => user.LastName).HasMaxLength(255);
+        }
+    }
+}
